9913c871a1d06e9b7ddd9444c2132dce40ae135d
AntonGordienkoDV <gordienkoav.dv@gmail.com>
2022-05-22 22:55:23 +0300

    Added working with directories

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import datetime
 
 
 class YaUploader:
@@ -15,14 +14,17 @@ class YaUploader:
             'Authorization': f'OAuth {self.token}'
         }
 
-    def create_folder(self):
-        url = f'{self.host}/v1/disk/resources'
-        headers = self.get_headers()
-        new_folder_path = '/upload_' + datetime.datetime.now().strftime("%d-%m-%Y_%H-%M")
-        params = {'path': new_folder_path}
-        response = requests.put(url, headers=headers, params=params)
-        if response.status_code in [201, 409]:
-            print(f'\nCreating folder {new_folder_path} -> OK\n')
+    def create_folder(self, parent_dir: str = '/', new_dir: str = ''):
+        if new_dir == '':
+            return parent_dir
+        else:
+            url = f'{self.host}/v1/disk/resources'
+            headers = self.get_headers()
+            new_folder_path = f'{parent_dir}/{new_dir}'
+            params = {'path': new_folder_path}
+            response = requests.put(url, headers=headers, params=params)
+            if response.status_code in [201, 409]:
+                print(f'\nCreating folder {new_folder_path} -> OK\n')
         return new_folder_path
 
     def _get_upload_link(self, path):
@@ -38,26 +40,26 @@ class YaUploader:
         response = requests.put(upload_link, data=open(file_name, 'rb'), headers=headers)
         response.raise_for_status()
         if response.status_code == 201:
-            print(f'Uploading file {file_name} -> OK')
+            print(f'Uploading file {file_name} -> OK\n')
 
-    def upload(self, file_path: str):
-        upload_folder_path = self.create_folder()
+    def upload(self, file_path: str, upload_folder_path: str = ''):
         if os.path.exists(file_path):
             if os.path.isfile(file_path):
+                print(f'Prepare file {file_path} to upload')
                 upload_file_name = upload_folder_path + '/' + os.path.basename(file_path)
                 self.upload_file(upload_file_name, file_path)
             elif os.path.isdir(file_path):
+                upload_folder_path = self.create_folder(upload_folder_path, os.path.basename(file_path))
                 files_list = os.listdir(file_path)
-                print(f'Prepare files in {file_path} to upload:')
-                print(*files_list, '', sep='\n')
                 for file_name in files_list:
-                    upload_file_name = upload_folder_path + '/' + file_name
-                    self.upload_file(upload_file_name, os.path.join(file_path, file_name))
+                    self.upload(os.path.join(file_path, file_name), upload_folder_path)
+        else:
+            print(f'Error! File or directory {file_path} is not found')
 
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = os.path.abspath(input('Enter path to file: ').strip())
+    path_to_file = os.path.abspath(input('Enter path to file or directory: ').strip())
     token = input('Enter your token: ').strip()
 
     uploader = YaUploader(token)
